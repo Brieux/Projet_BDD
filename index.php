9f0055836099b1d5e956b47fcb081c04c3da6eaa
jraynaud <jraynaud@v17.fdsetu.infra.umontpellier.fr>
2018-11-14 12:52:04 +0100

    modif index

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div id="MenuIC">
+    Inscription:
+    <form action="inscription.php"method="post">
+      Email:<input type = "email" id="email" value="Email here"><br/>
+      Pseudo:<input type = "text" id="pseudo" value="Pseudo"><br/>
+      Password:<input type="password" id="pass" value="meh"><br/>
+      <input type="submit" value="S'inscrire">
+    </form>
+    <br/>
+    Connexion:
+    <form action="connexion.php"method="post">
+      Pseudo:<input type = "text" id="pseudo" value="Pseudo"><br/>
+      Password:<input type="password" id="pass" value="meh"><br/>
+      <input type="submit" value="Se connecter">   
+    </form>
+  </div>
+  <div id="Menu">
+    <ul>
+      <li><a href="oui.php">Oui</a></li>
+      <li><a href="oui.php">Oui</a></li>
+      <li><a href="oui.php">Oui</a></li>
+      <li><a href="oui.php">Oui</a></li>
+      <li><a href="oui.php">Oui</a></li>
+    </ul>
+  </div>
+
+</body>
+</html>
