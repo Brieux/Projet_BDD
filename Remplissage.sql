6c395e1b1ad40fa3d4207dc4c5e8966b67b4a304
jraynaud <jraynaud@i2.fdsetu.infra.umontpellier.fr>
2018-12-19 17:17:23 +0100

    OUI

--- Remplissage.sql
+++ Remplissage.sql
@@ -17,10 +17,10 @@ INSERT INTO MEMBRES VALUES ('jackson@gmail.com','sg2456q','Jackson','Ville','Jac
 INSERT INTO MEMBRES VALUES ('germain@gmail.com','fr78as4','Saint','Germain','Saint-Germain','36 rue du marechal Petain',0125478956,'0','ACTIVE');
 INSERT INTO MEMBRES VALUES ('pellier@gmail.com','98ae7sq','Mont','Pellier','Montpellier','01 bis allée mario gustavi',0457461238,'0','ACTIVE');
 
-INSERT INTO BIEN VALUES(111111,'Turbine', 5475,'jackson@gmail.com','germain@gmail.com');
-INSERT INTO BIEN VALUES(111112,'Soupape admission', 875,'jackson@gmail.com','germain@gmail.com');
-INSERT INTO BIEN VALUES(111113,'Stylo bic', 2,'jackson@gmail.com','pellier@gmail.com');
-INSERT INTO BIEN VALUES(111114,'Grenouillere', 45,'pellier@gmail.com','germain@gmail.com');
+INSERT INTO BIEN VALUES(111111,'Turbine', 5475,'jackson@gmail.com','germain@gmail.com','PRIS');
+INSERT INTO BIEN VALUES(111112,'Soupape admission', 875,'jackson@gmail.com','germain@gmail.com','PRIS');
+INSERT INTO BIEN VALUES(111113,'Stylo bic', 2,'jackson@gmail.com','pellier@gmail.com','PRIS');
+INSERT INTO BIEN VALUES(111114,'Grenouillere', 45,'pellier@gmail.com','germain@gmail.com','PRIS');
 
 INSERT INTO SERVICES VALUES(11111111,'Jardinnage', '2019-01-12 08:00:00', '2019-01-12 17:30:00',30, 'Possibilité de s"occuper de votre jardin et de faire pousser de jolies plantes','pellier@gmail.com','germain@gmail.com');
 INSERT INTO SERVICES VALUES(11111112,'Babysitting', '2019-02-01 17:00:00', '2019-02-01 23:30:00',15, 'Babysitting tout age','pellier@gmail.com','germain@gmail.com');
--- trigger.sql
+++ trigger.sql
@@ -1,33 +1,31 @@
-create or replace 
-trigger tBien
-before insert or update on bien
-for each row
-begin
-if INSERTING THEN
-    if (select etat from membres where email = :new.EMAIL_CREAT) == 'INACTIVE' THEN
-        raise_application_error(-20002, 'insertion impossible');  
-    end if; 
-end if;
-if UPDATING THEN
-    if (select etat from membres where email = :new.EMAIL_USER) == 'INACTIVE' THEN
-        raise_application_error(-20003, 'modification impossible');  
-    end if; 
-end if;
-end;
+set serveroutput on
+--creation d'un table log pour retenir les modifcation
+create table log (email varchar(60), date_insertion date, 
+usager varchar(20),typeOperation varchar(12));
 
-create or replace 
-trigger tService
-before insert or update on service
+create or replace trigger tLog
+after insert or update or delete on membres
 for each row
-begin
-if INSERTING THEN
-    if (select etat from membres where email = :new.EMAIL_CREAT) == 'INACTIVE' THEN
-        raise_application_error(-20002, 'insertion impossible');  
-    end if; 
-end if;
-if UPDATING THEN
-    if (select etat from membres where email = :new.EMAIL_USER) == 'INACTIVE' THEN
-        raise_application_error(-20003, 'modification impossible');  
-    end if; 
+begin--on veut retenir l'email du membre modifié dans la table log
+if inserting--a l'insertion
+then
+insert into log values(:new.email,sysdate,user,'insertion');
+elsif updating--a la modification
+then
+insert into log values(:new.email,sysdate,user,'modification');
+elsif deleting--a la supression
+then
+insert into log values(:old.email,sysdate,user,'supression');
 end if;
 end;
+/
+
+create or replace trigger tBien
+before delete on bien
+for each row
+begin
+if :old.etat = 'PRIS' then --si l'objet est emprunté, on ne peut pas le supprimer
+    raise_application_error(-20003,'Supression impossible');
+end if;
+end;
+/
